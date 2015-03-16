pd3/bcftools

commit bcc7b26ec34d060e11b8fac555c1df86a6d358b5

Do not use 0x80 & co for missing alleles

mcall.c
@@ -918,7 +918,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         for (i=0; i<ngts; i++) if ( pdg[i]!=0.0 ) break;
         if ( i==ngts || !ploidy )
         {
-            gts[0] = bcf_int32_missing;
+            gts[0] = bcf_gt_missing;
             gls[0] = 1;
             continue;
         }
@@ -1056,7 +1056,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
             int32_t *gts = call->cgts + ismpl;
             if ( gl[0]==1 || igt==GT_SKIP )    // zero depth, set missing genotypes
             {
-                gts[0] = bcf_int32_missing;
+                gts[0] = bcf_gt_missing;
                 // bcf_float_set_missing(call->GQs[ismpl]);
                 continue;
             }
@@ -1106,7 +1106,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         cgts++;
         ugts++;
         gts += 2;
-        if ( ugts[0]==bcf_int32_missing )
+        if ( bcf_gt_is_missing(ugts[0]) )
         {
             gts[0] = bcf_gt_missing;
             gts[1] = ploidy==2 ? bcf_gt_missing : bcf_int32_vector_end;

consensus.c
@@ -214,14 +214,14 @@ static void apply_variant(args_t *args, bcf1_t *rec)
             if ( args->haplotype > fmt->n ) error("Can't apply %d-th haplotype at %s:%d\n", args->haplotype,bcf_seqname(args->hdr,rec),rec->pos+1);
             uint8_t *ignore, *ptr = fmt->p + fmt->size*args->isample + args->haplotype - 1;
             ialt = bcf_dec_int1(ptr, fmt->type, &ignore);
-            if ( !ialt || ialt==bcf_int32_missing || ialt==bcf_int32_vector_end ) return;
+            if ( bcf_gt_is_missing(ialt) || ialt==bcf_int32_vector_end ) return;
             ialt = bcf_gt_allele(ialt);
         }
         else if ( args->output_iupac ) 
         {
             uint8_t *ignore, *ptr = fmt->p + fmt->size*args->isample;
             ialt = bcf_dec_int1(ptr, fmt->type, &ignore);
-            if ( !ialt || ialt==bcf_int32_missing || ialt==bcf_int32_vector_end ) return;
+            if ( bcf_gt_is_missing(ialt) || ialt==bcf_int32_vector_end ) return;
             ialt = bcf_gt_allele(ialt);
 
             int jalt;
@@ -229,7 +229,7 @@ static void apply_variant(args_t *args, bcf1_t *rec)
             {
                 ptr = fmt->p + fmt->size*args->isample + 1;
                 jalt = bcf_dec_int1(ptr, fmt->type, &ignore);
-                if ( !jalt || jalt==bcf_int32_missing || jalt==bcf_int32_vector_end ) jalt = ialt;
+                if ( bcf_gt_is_missing(jalt) || jalt==bcf_int32_vector_end ) jalt = ialt;
                 else jalt = bcf_gt_allele(jalt);
             }
             else jalt = ialt;
@@ -247,7 +247,7 @@ static void apply_variant(args_t *args, bcf1_t *rec)
             {
                 uint8_t *ignore, *ptr = fmt->p + fmt->size*args->isample + i;
                 ialt = bcf_dec_int1(ptr, fmt->type, &ignore);
-                if ( !ialt || ialt==bcf_int32_missing || ialt==bcf_int32_vector_end ) return;
+                if ( bcf_gt_is_missing(ialt) || ialt==bcf_int32_vector_end ) return;
                 ialt = bcf_gt_allele(ialt);
                 if ( ialt ) break;
             }

convert.c
@@ -420,7 +420,7 @@ static void process_gt_to_prob3(convert_t *convert, bcf1_t *line, fmt_t *fmt, in
         else if ( j==1 )
         {
             // haploid
-            if ( ptr[0]==bcf_gt_missing )
+            if ( bcf_gt_is_missing(ptr[0]) )
                 kputs(" 0.5 0.0 0.5", str);
             else if ( bcf_gt_allele(ptr[0])==1 )
                 kputs(" 0 0 1", str);       // first ALT allele
@@ -539,7 +539,7 @@ static void process_gt_to_hap(convert_t *convert, bcf1_t *line, fmt_t *fmt, int 
         if ( j==2 )
         {
             // diploid
-            if ( ptr[0]==bcf_gt_missing || ptr[1]==bcf_gt_missing ) {
+            if ( bcf_gt_is_missing(ptr[0]) || bcf_gt_is_missing(ptr[1]) ) {
                 kputs("? ?", str);
             }
             else if ( bcf_gt_is_phased(ptr[1])) {
@@ -552,7 +552,7 @@ static void process_gt_to_hap(convert_t *convert, bcf1_t *line, fmt_t *fmt, int 
         else if ( j==1 )
         {
             // haploid
-            if ( ptr[0]==bcf_gt_missing )
+            if ( bcf_gt_is_missing(ptr[0]) )
                 kputs("? -", str);
             else if ( bcf_gt_allele(ptr[0])==1 )
                 kputs("1 -", str);       // first ALT allele
@@ -586,7 +586,7 @@ static void process_gt_to_hap2(convert_t *convert, bcf1_t *line, fmt_t *fmt, int
         if ( j==2 )
         {
             // diploid
-            if ( ptr[0]==bcf_gt_missing || ptr[1]==bcf_gt_missing ) {
+            if ( bcf_gt_is_missing(ptr[0]) || bcf_gt_is_missing(ptr[1]) ) {
                 kputs("? ?", str);
             }
             else if ( bcf_gt_is_phased(ptr[1])) {
@@ -599,7 +599,7 @@ static void process_gt_to_hap2(convert_t *convert, bcf1_t *line, fmt_t *fmt, int
         else if ( j==1 )
         {
             // haploid
-            if ( ptr[0]==bcf_gt_missing )
+            if ( bcf_gt_is_missing(ptr[0]) )
                 kputs("? ?", str);
             else if ( bcf_gt_allele(ptr[0])==1 )
                 kputs("1 1", str);       // first ALT allele

vcfannotate.c
@@ -537,8 +537,8 @@ static int vcf_setter_format_gt(args_t *args, bcf1_t *line, annot_col_t *col, vo
             if ( args->sample_map[i]==-1 ) continue;
             int32_t *src = args->tmpi  + nsrc*args->sample_map[i];
             int32_t *dst = args->tmpi2 + ndst*i;
-            if ( col->replace==REPLACE_EXISTING && dst[0]==bcf_gt_missing ) continue;
-            if ( col->replace==REPLACE_MISSING  && dst[0]!=bcf_gt_missing ) continue;
+            if ( col->replace==REPLACE_EXISTING && bcf_gt_is_missing(dst[0]) ) continue;
+            if ( col->replace==REPLACE_MISSING  && !bcf_gt_is_missing(dst[0]) ) continue;
             for (j=0; j<nsrc; j++) dst[j] = src[j];
             for (; j<ndst; j++) dst[j] = bcf_int32_vector_end;
         }
@@ -553,8 +553,8 @@ static int vcf_setter_format_gt(args_t *args, bcf1_t *line, annot_col_t *col, vo
             int32_t *dst = args->tmpi3 + nsrc*i;
             int keep_ori = 0;
             if ( args->sample_map[i]==-1 ) keep_ori = 1;
-            else if ( col->replace==REPLACE_EXISTING && ori[0]==bcf_gt_missing ) keep_ori = 1;
-            else if ( col->replace==REPLACE_MISSING  && ori[0]!=bcf_gt_missing ) keep_ori = 1;
+            else if ( col->replace==REPLACE_EXISTING && bcf_gt_is_missing(ori[0]) ) keep_ori = 1;
+            else if ( col->replace==REPLACE_MISSING  && !bcf_gt_is_missing(ori[0]) ) keep_ori = 1;
             if ( keep_ori )
             {
                 for (j=0; j<ndst; j++) dst[j] = ori[j];

vcfconcat.c
@@ -189,7 +189,7 @@ static void phase_update(args_t *args, bcf_hdr_t *hdr, bcf1_t *rec)
     {
         if ( !args->swap_phase[i] ) continue;
         int *gt = &args->GTa[i*2];
-        if ( gt[0]==bcf_gt_missing || gt[1]==bcf_int32_vector_end ) continue;
+        if ( bcf_gt_is_missing(gt[0]) || gt[1]==bcf_int32_vector_end ) continue;
         SWAP(int, gt[0], gt[1]);
         gt[1] |= 1;
     }
@@ -222,7 +222,7 @@ static void phased_flush(args_t *args)
             int *gta = &args->GTa[j*2];
             int *gtb = &args->GTb[j*2];
             if ( gta[1]==bcf_int32_vector_end || gtb[1]==bcf_int32_vector_end ) continue;
-            if ( gta[0]==bcf_gt_missing || gta[1]==bcf_gt_missing || gtb[0]==bcf_gt_missing || gtb[1]==bcf_gt_missing ) continue;
+            if ( bcf_gt_is_missing(gta[0]) || bcf_gt_is_missing(gta[1]) || bcf_gt_is_missing(gtb[0]) || bcf_gt_is_missing(gtb[1]) ) continue;
             if ( !bcf_gt_is_phased(gta[1]) || !bcf_gt_is_phased(gtb[1]) ) continue;
             if ( bcf_gt_allele(gta[0])==bcf_gt_allele(gta[1]) || bcf_gt_allele(gtb[0])==bcf_gt_allele(gtb[1]) ) continue;
             if ( bcf_gt_allele(gta[0])==bcf_gt_allele(gtb[0]) && bcf_gt_allele(gta[1])==bcf_gt_allele(gtb[1]) )

vcfgtcheck.c
@@ -310,16 +310,15 @@ static int fake_PLs(args_t *args, bcf_hdr_t *hdr, bcf1_t *line)
     for (i=0; i<bcf_hdr_nsamples(hdr); i++)
     {
         int *gt_ptr = args->tmp_arr + i*nsm_gt;
-        int a = bcf_gt_allele(gt_ptr[0]);
-        int b = bcf_gt_allele(gt_ptr[1]);
-
         int j, *pl_ptr = args->pl_arr + i*npl;
-        if ( a<0 || b<0 ) // missing genotype
+        if ( bcf_gt_is_missing(gt_ptr[0]) || bcf_gt_is_missing(gt_ptr[1]) ) // missing genotype
         {
             for (j=0; j<npl; j++) pl_ptr[j] = -1;
         }
         else
         {
+            int a = bcf_gt_allele(gt_ptr[0]);
+            int b = bcf_gt_allele(gt_ptr[1]);
             for (j=0; j<npl; j++) pl_ptr[j] = fake_PL;
             int idx = bcf_alleles2gt(a,b);
             pl_ptr[idx] = 0;
@@ -434,9 +433,9 @@ static void check_gt(args_t *args)
         {
             int *gt_ptr = gt_arr + i*ngt;
             if ( gt_ptr[1]==bcf_int32_vector_end ) continue;    // skip haploid genotypes
+            if ( bcf_gt_is_missing(gt_ptr[0]) || bcf_gt_is_missing(gt_ptr[1]) ) continue;
             int a = bcf_gt_allele(gt_ptr[0]);
             int b = bcf_gt_allele(gt_ptr[1]);
-            if ( a<0 || b<0 ) continue; // missing genotypes
             if ( args->hom_only && a!=b ) continue; // heterozygous genotype
             int igt_tgt = igt_tgt = bcf_alleles2gt(a,b); // genotype index in the target file
             int igt_qry = gt2ipl[igt_tgt];  // corresponding genotype in query file

vcfmerge.c
@@ -1151,7 +1151,7 @@ void merge_GT(args_t *args, bcf_fmt_t **fmt_map, bcf1_t *out)
             continue;
         }
 
-        #define BRANCH(type_t, missing, vector_end) { \
+        #define BRANCH(type_t, vector_end) { \
             type_t *p_ori  = (type_t*) fmt_ori->p; \
             if ( !ma->d[i][0].als_differ ) \
             { \
@@ -1162,7 +1162,7 @@ void merge_GT(args_t *args, bcf_fmt_t **fmt_map, bcf1_t *out)
                     { \
                         if ( p_ori[k]==vector_end ) break; /* smaller ploidy */ \
                         ma->smpl_ploidy[ismpl+j]++; \
-                        if ( p_ori[k]==missing ) tmp[k] = 0; /* missing allele */ \
+                        if ( bcf_gt_is_missing(p_ori[k]) ) tmp[k] = 0; /* missing allele */ \
                         else tmp[k] = p_ori[k]; \
                     } \
                     for (; k<nsize; k++) tmp[k] = bcf_int32_vector_end; \
@@ -1179,7 +1179,7 @@ void merge_GT(args_t *args, bcf_fmt_t **fmt_map, bcf1_t *out)
                 { \
                     if ( p_ori[k]==vector_end ) break; /* smaller ploidy */ \
                     ma->smpl_ploidy[ismpl+j]++; \
-                    if ( !(p_ori[k]>>1) || p_ori[k]==missing ) tmp[k] = 0; /* missing allele */ \
+                    if ( bcf_gt_is_missing(p_ori[k]) ) tmp[k] = 0; /* missing allele */ \
                     else \
                     { \
                         int al = (p_ori[k]>>1) - 1; \
@@ -1195,9 +1195,9 @@ void merge_GT(args_t *args, bcf_fmt_t **fmt_map, bcf1_t *out)
         }
         switch (fmt_ori->type)
         {
-            case BCF_BT_INT8: BRANCH(int8_t,   bcf_int8_missing,  bcf_int8_vector_end); break;
-            case BCF_BT_INT16: BRANCH(int16_t, bcf_int16_missing, bcf_int16_vector_end); break;
-            case BCF_BT_INT32: BRANCH(int32_t, bcf_int32_missing, bcf_int32_vector_end); break;
+            case BCF_BT_INT8: BRANCH(int8_t,   bcf_int8_vector_end); break;
+            case BCF_BT_INT16: BRANCH(int16_t, bcf_int16_vector_end); break;
+            case BCF_BT_INT32: BRANCH(int32_t, bcf_int32_vector_end); break;
             default: error("Unexpected case: %d\n", fmt_ori->type);
         }
         #undef BRANCH

vcfnorm.c
@@ -804,8 +804,7 @@ static void split_format_genotype(args_t *args, bcf1_t *src, bcf_fmt_t *fmt, int
         for (j=0; j<ngts; j++)
         {
             if ( gt[j]==bcf_int32_vector_end ) break;
-            if ( gt[j]==bcf_int32_missing && bcf_gt_allele(gt[j])==0 ) continue;
-            if ( gt[j]==0 ) continue;   // missing allele: leave as is
+            if ( bcf_gt_is_missing(gt[j]) || bcf_gt_allele(gt[j])==0 ) continue; // missing allele or ref: leave as is
             if ( bcf_gt_allele(gt[j])==ialt+1 )
                 gt[j] = bcf_gt_unphased(1) | bcf_gt_is_phased(gt[j]); // set to first ALT
             else
@@ -1260,7 +1259,7 @@ static void merge_format_genotype(args_t *args, bcf1_t **lines, int nlines, bcf_
             for (k=0; k<ngts; k++)
             {
                 if ( gt2[k]==bcf_int32_vector_end ) break;
-                if ( gt2[k]==bcf_int32_missing || bcf_gt_allele(gt2[k])==0 ) continue;
+                if ( bcf_gt_is_missing(gt2[k]) || bcf_gt_allele(gt2[k])==0 ) continue;
                 if ( gt2[k]==0 ) gt[k] = 0; // missing genotype
                 else
                 {

commit eb0675f3019576802b58b3bb651d0f9b05b2ffd2

Fixed bug in trio calling introduced by bcc7b26e

mcall.c
@@ -899,10 +899,11 @@ static void mcall_call_genotypes(call_t *call, bcf1_t *rec, int nals, int nout_a
 static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int nout_als, int out_als)
 {
     int ia, ib, i;
-    int nsmpl   = bcf_hdr_nsamples(call->hdr);
-    int ngts    = nals*(nals+1)/2;
-    double *gls = call->GLs - ngts;
-    double *pdg = call->pdg - ngts;
+    int nsmpl    = bcf_hdr_nsamples(call->hdr);
+    int ngts     = nals*(nals+1)/2;
+    int nout_gts = nout_als*(nout_als+1)/2;
+    double *gls  = call->GLs - nout_gts;
+    double *pdg  = call->pdg - ngts;
 
     // Calculate individuals' genotype likelihoods P(X=i)
     int isample;
@@ -911,19 +912,19 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         int ploidy = call->ploidy ? call->ploidy[isample] : 2;
         int32_t *gts = call->ugts + isample;
 
-        gls += ngts;
+        gls += nout_gts;
         pdg += ngts;
 
         // Skip samples with all pdg's equal to 1. These have zero depth.
         for (i=0; i<ngts; i++) if ( pdg[i]!=0.0 ) break;
         if ( i==ngts || !ploidy )
         {
-            gts[0] = bcf_gt_missing;
+            gts[0] = -1;
             gls[0] = 1;
             continue;
         }
 
-        for (i=0; i<ngts; i++) gls[i] = -HUGE_VAL;
+        for (i=0; i<nout_gts; i++) gls[i] = -HUGE_VAL;
 
         double sum_lk  = 0;
         double best_lk = 0;
@@ -962,7 +963,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
                 }
             }
         }
-        for (i=0; i<ngts; i++)
+        for (i=0; i<nout_gts; i++)
             if ( gls[i]!=-HUGE_VAL ) gls[i] = log(gls[i]/sum_lk);
     }
 
@@ -1001,11 +1002,11 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         for (i=0; i<3; i++)     // for father, mother, child
         {
             int ismpl = fam->sample[i];
-            double *gl = call->GLs + ngts*ismpl;
+            double *gl = call->GLs + nout_gts*ismpl;
             if ( gl[0]==1 ) continue;
             int j, jmax = 0;
             double max  = gl[0];
-            for (j=1; j<ngts; j++)
+            for (j=1; j<nout_gts; j++)
                 if ( max < gl[j] ) { max = gl[j]; jmax = j; }
             uc_lk += max;
             uc_itr |= jmax << ((2-i)*4);
@@ -1021,7 +1022,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
             for (i=0; i<3; i++)     // for father, mother, child
             {
                 int ismpl = fam->sample[i];
-                double *gl = call->GLs + ngts*ismpl;
+                double *gl = call->GLs + nout_gts*ismpl;
                 if ( gl[0]==1 ) continue;
                 int igt = trio[itr]>>((2-i)*4) & 0xf;
                 assert( !call->ploidy || call->ploidy[ismpl]>0 );
@@ -1041,10 +1042,10 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         if ( !uc_is_mendelian )
         {
             uc_lk += log(1 - trio_Pm);
-            //fprintf(stderr,"c_lk=%e uc_lk=%e c_itr=%d%d%d uc_itr=%d%d%d\n", c_lk,uc_lk,c_itr>>8&0xf,c_itr>>4&0xf,c_itr&0xf,uc_itr>>8&0xf,uc_itr>>4&0xf,uc_itr&0xf);
+            // fprintf(stderr,"c_lk=%e uc_lk=%e c_itr=%d%d%d uc_itr=%d%d%d\n", c_lk,uc_lk,c_itr>>8&0xf,c_itr>>4&0xf,c_itr&0xf,uc_itr>>8&0xf,uc_itr>>4&0xf,uc_itr&0xf);
             if ( c_lk < uc_lk ) { c_lk = uc_lk; c_itr = uc_itr; }
         }
-        //fprintf(stderr,"best_lk=%e best_itr=%d%d%d uc_itr=%d%d%d\n", c_lk,c_itr>>8&0xf,c_itr>>4&0xf,c_itr&0xf,uc_itr>>8&0xf,uc_itr>>4&0xf,uc_itr&0xf);
+        // fprintf(stderr,"best_lk=%e best_itr=%d%d%d uc_itr=%d%d%d\n", c_lk,c_itr>>8&0xf,c_itr>>4&0xf,c_itr&0xf,uc_itr>>8&0xf,uc_itr>>4&0xf,uc_itr&0xf);
 
         // Set genotypes for father, mother, child and calculate genotype qualities
         for (i=0; i<3; i++)
@@ -1052,11 +1053,11 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
             // GT
             int ismpl    = fam->sample[i];
             int igt      = c_itr>>((2-i)*4) & 0xf;
-            double *gl   = call->GLs + ngts*ismpl;
+            double *gl   = call->GLs + nout_gts*ismpl;
             int32_t *gts = call->cgts + ismpl;
             if ( gl[0]==1 || igt==GT_SKIP )    // zero depth, set missing genotypes
             {
-                gts[0] = bcf_gt_missing;
+                gts[0] = -1;
                 // bcf_float_set_missing(call->GQs[ismpl]);
                 continue;
             }
@@ -1106,7 +1107,7 @@ static void mcall_call_trio_genotypes(call_t *call, bcf1_t *rec, int nals, int n
         cgts++;
         ugts++;
         gts += 2;
-        if ( bcf_gt_is_missing(ugts[0]) )
+        if ( ugts[0]==-1 )
         {
             gts[0] = bcf_gt_missing;
             gts[1] = ploidy==2 ? bcf_gt_missing : bcf_int32_vector_end;