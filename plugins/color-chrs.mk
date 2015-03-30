3dca9379e595901d600a8f549853da153392e3b0
Petr Danecek <pd3@sanger.ac.uk>
2015-03-30 14:35:12 +0100

    Updated diverged plugin rule

--- plugins/color-chrs.mk
+++ plugins/color-chrs.mk
@@ -1,2 +1,2 @@
 plugins/color-chrs.so: plugins/color-chrs.c version.h version.c HMM.h HMM.c $(htslib_shared)
-	$(CC) $(CFLAGS) $(INCLUDES) -fPIC -shared -o $@ HMM.c version.c $< -L$(HTSDIR) -lhts
+	$(CC) -fPIC -shared $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) -L$(HTSDIR) $(LDFLAGS) -o $@ HMM.c version.c $< -lhts $(LIBS)
