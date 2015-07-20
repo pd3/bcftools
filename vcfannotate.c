pd3/bcftools

commit a66d553bf42d97e5fe98293815fcd9c635d43f61

Informative error message: annotate supports FMT fields only from a VCF

vcfannotate.c
@@ -1243,8 +1243,11 @@ static void init_columns(args_t *args)
                     }
             }
         }
-        else if ( args->tgts_is_vcf && (!strncasecmp("FORMAT/",str.s, 7) || !strncasecmp("FMT/",str.s,4)) )
+        else if ( !strncasecmp("FORMAT/",str.s, 7) || !strncasecmp("FMT/",str.s,4) )
         {
+            if ( args->tgts_is_vcf )
+                error("Error: FORMAT fields can be carried over from a VCF file only.\n");
+
             char *key = str.s + (!strncasecmp("FMT/",str.s,4) ? 4 : 7);
             if ( force_samples<0 ) force_samples = replace;
             if ( force_samples>=0 && replace!=REPLACE_ALL ) force_samples = replace;;

commit d8c1e2d6d82ce67c0b9e487ae87bbad5669634f6

Fixed a typo introduced by a66d553

vcfannotate.c
@@ -1245,7 +1245,7 @@ static void init_columns(args_t *args)
         }
         else if ( !strncasecmp("FORMAT/",str.s, 7) || !strncasecmp("FMT/",str.s,4) )
         {
-            if ( args->tgts_is_vcf )
+            if ( !args->tgts_is_vcf )
                 error("Error: FORMAT fields can be carried over from a VCF file only.\n");
 
             char *key = str.s + (!strncasecmp("FMT/",str.s,4) ? 4 : 7);