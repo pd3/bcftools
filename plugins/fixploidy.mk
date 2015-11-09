5f1756ace33f8779dee71c10bad7fd3e1349558b
John Marshall <jm18@sanger.ac.uk>
2015-11-09 16:26:31 +0000

    Dynamically export bcftools symbols to plugins
    ...instead of linking the plugins against libhts.so.  This simplifies
    building and installation, as plugins no longer need to be able to find
    libhts.so/.dylib at runtime -- which to date has unfortunately often
    been done via $LD_LIBRARY_PATH.
    
    On OS X, build plugins as Mach-O bundles rather than shared libraries
    (and ensure that bcftools is built first, so is available for use with
    -bundle_loader).  Fixes #225.
    
    (TODO) The advice in print_plugin_usage_hint() should be revisited.
    
    When bcftools is linked to libhts.a statically, there is a small chance
    that a plugin will need HTSlib functions that have not been pulled in by
    the bcftools executable.  This is surely unlikely, and can be prevented
    by linking bcftools or your plugin against a shared libhts.so.
    
    Fixes #249; fixes #224; supersedes and closes #254.

--- plugins/fixploidy.mk
+++ plugins/fixploidy.mk
@@ -1,2 +1,2 @@
-plugins/fixploidy.so: plugins/fixploidy.c version.h version.c ploidy.h ploidy.c $(htslib_shared)
-	$(CC) -fPIC -shared $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) -L$(HTSDIR) $(LDFLAGS) -o $@ ploidy.c version.c $< -lhts $(LIBS)
+plugins/fixploidy.so: plugins/fixploidy.c version.h version.c ploidy.h ploidy.c
+	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) $(LDFLAGS) -o $@ ploidy.c version.c $< $(LIBS)
--- plugins/vcf2sex.mk
+++ plugins/vcf2sex.mk
@@ -1,2 +1,2 @@
-plugins/vcf2sex.so: plugins/vcf2sex.c version.h version.c ploidy.h ploidy.c $(htslib_shared)
-	$(CC) -fPIC -shared $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) -L$(HTSDIR) $(LDFLAGS) -o $@ ploidy.c version.c $< -lhts $(LIBS)
+plugins/vcf2sex.so: plugins/vcf2sex.c version.h version.c
+	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) $(LDFLAGS) -o $@ version.c $< $(LIBS)
