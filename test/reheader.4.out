pd3/bcftools

commit 169d700769e0acffd1df71faa26a6a3217fe1487

reheader supports spaces in sample names

reheader.c
@@ -69,22 +69,41 @@ static void read_header_file(char *fname, kstring_t *hdr)
 static int set_sample_pairs(char **samples, int nsamples, kstring_t *hdr, int idx)
 {
     int i, j, n;
+    kstring_t key = {0,0,0};
+    kstring_t val = {0,0,0};
 
     // Are these samples "old-name new-name" pairs?
     void *hash = khash_str2str_init();
     for (i=0; i<nsamples; i++)
     {
-        char *key, *value;
-        key = value = samples[i];
-        while ( *value && !isspace(*value) ) value++;
-        if ( !*value ) break;
-        *value = 0; value++;
-        while ( isspace(*value) ) value++;
-        khash_str2str_set(hash,key,value);
+        char *ptr = samples[i];
+        key.l = val.l = 0;
+        int escaped = 0;
+        while ( *ptr )
+        {
+            if ( *ptr=='\\' && !escaped ) { escaped = 1; ptr++; continue; }
+            if ( isspace(*ptr) && !escaped ) break;
+            kputc(*ptr, &key);
+            escaped = 0;
+            ptr++;
+        }
+        if ( !key.l ) break;
+        ptr++;
+        while ( *ptr )
+        {
+            if ( *ptr=='\\' && !escaped ) { escaped = 1; ptr++; continue; }
+            if ( isspace(*ptr) && !escaped ) break;
+            kputc(*ptr, &val);
+            escaped = 0;
+            ptr++;
+        }
+        khash_str2str_set(hash,strdup(key.s),strdup(val.s));
     }
+    free(key.s);
+    free(val.s);
     if ( i!=nsamples )  // not "old-name new-name" pairs
     {
-        khash_str2str_destroy(hash);
+        khash_str2str_destroy_free_all(hash);
         return 0;
     }
 
@@ -117,7 +136,7 @@ static int set_sample_pairs(char **samples, int nsamples, kstring_t *hdr, int id
     char *ori = khash_str2str_get(hash,hdr->s+idx+j);
     kputs(ori ? ori : hdr->s+idx+j, &tmp);
 
-    if ( hash ) khash_str2str_destroy(hash);
+    khash_str2str_destroy_free_all(hash);
 
     hdr->l = idx;
     kputs(tmp.s, hdr);

khash_str2str.h
@@ -61,6 +61,23 @@ static inline void khash_str2str_destroy_free(void *_hash)
 }
 
 /*
+ *  Destroys the hash structure, the keys and the values
+ */
+static inline void khash_str2str_destroy_free_all(void *_hash)
+{
+    khash_t(str2str) *hash = (khash_t(str2str)*)_hash;
+    khint_t k;
+    if (hash == 0) return;
+    for (k = 0; k < kh_end(hash); ++k)
+        if (kh_exist(hash, k))
+        {
+            free((char*)kh_key(hash, k));
+            free((char*)kh_val(hash, k));
+        }
+    kh_destroy(str2str, hash);
+}
+
+/*
  *  Returns value if key exists or NULL if not
  */
 static inline char *khash_str2str_get(void *_hash, const char *str)

commit 8fc75c5bac3afef73ef79de07c3571d625cff214

Fixed bug introduced by 169d700
- make reheader work again with a non-paired list of samples
- added tests

reheader.c
@@ -87,7 +87,7 @@ static int set_sample_pairs(char **samples, int nsamples, kstring_t *hdr, int id
             escaped = 0;
             ptr++;
         }
-        if ( !key.l ) break;
+        if ( !*ptr ) break;
         ptr++;
         while ( *ptr )
         {

test/reheader.3.out
@@ -1,0 +1,55 @@
+##fileformat=VCFv4.1
+##FILTER=<ID=PASS,Description="All filters passed">
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+##INFO=<ID=XRF,Number=R,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAF,Number=A,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGF,Number=G,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XRI,Number=R,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAI,Number=A,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGI,Number=G,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XRS,Number=R,Type=String,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAS,Number=A,Type=String,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGS,Number=G,Type=String,Description="Test Number=AGR in INFO">
+##FORMAT=<ID=FRF,Number=R,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAF,Number=A,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGF,Number=G,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FRI,Number=R,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAI,Number=A,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGI,Number=G,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FRS,Number=R,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAS,Number=A,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGS,Number=G,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FSTR,Number=1,Type=String,Description="Test String in FORMAT">
+##INFO=<ID=ISTR,Number=1,Type=String,Description="Test String in INFO">
+##INFO=<ID=INDEL,Number=0,Type=Flag,Description="Indicates that the variant is an INDEL.">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Phred-scaled likelihood">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Depth">
+##contig=<ID=1,length=2147483647>
+##contig=<ID=2,length=2147483647>
+##contig=<ID=3,length=2147483647>
+##contig=<ID=4,length=2147483647>
+##contig=<ID=5,length=2147483647>
+##contig=<ID=20,length=2147483647>
+##FILTER=<ID=Test,Description="Test Filter">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	A AA	BB B
+2	101	.	ATTTTTTTTTTTTT	ATTTTTTTTTTTTTTT	999	PASS	INDEL;AN=4;AC=4	GT:DP	1/1:1	1/1:1
+2	114	.	TC	TTCC,TTC	999	PASS	INDEL;AN=4;AC=2,2	GT:DP	1/2:1	1/2:1
+2	115	.	C	T	999	PASS	INDEL;AN=4;AC=4	GT:DP	1/1:1	1/1:1
+20	3	.	G	CT	999	PASS	INDEL;AN=4;AC=2	GT	0/1	0/1
+20	3	.	GATG	GACT	999	PASS	INDEL;AN=4;AC=2	GT	1/0	1/0
+20	5	.	TGGG	TAC,TG,TGGGG,AC	.	PASS	INDEL;AN=4;AC=2,2,0,0	GT:PL:DP	1/2:1,2,3,4,5,6,7,8,9,10,11,12,13,14,15:1	1/2:1,2,3,4,5,6,7,8,9,10,11,12,13,14,15:1
+20	59	.	AG	.	999	PASS	AN=4	GT:PL:DP	0/0:0:4	0/0:0:4
+20	80	.	CACAG	CACAT	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	81	.	A	C	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	95	.	TCACCG	ACACCG	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	95	.	TCACCG	AAAAAA	999	Test	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	273	.	CAAAAAAAAAAAAAAAAAAAAA	CAAAAAAAAAAAAAAAAAAAAAAA,CAAAAAAAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=4;AC=2,2	GT:PL:DP	1/2:0,3,5,3,5,5:1	1/2:0,3,5,3,5,5:1
+20	274	.	AAAAAAAAA	AAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=0;AC=0	GT:PL:DP	./.:0,0,0:0	./.:0,0,0:0
+20	278	.	AAAAAAAAAAAAAAAAA	AAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=0;AC=0	GT:PL:DP	./.:0,0,0:0	./.:0,0,0:0
+3	10	.	GTGGAC	GTGGACACAC,GTGGACAC,GTGGACACACAC,GTGG,GTGGACACACACAC,ATGGACACACAC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+3	15	.	CACA	CAC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+4	21	.	ATTTTTTTTTTTTTTTC	ATTTTTTTTTTTTTTC,ATTTTTTTTTTTTTTTT,ATTTTTTTTTTTTTTTTC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+5	22	.	A	AGA	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0

test/reheader.4.out
@@ -1,0 +1,55 @@
+##fileformat=VCFv4.1
+##FILTER=<ID=PASS,Description="All filters passed">
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+##INFO=<ID=XRF,Number=R,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAF,Number=A,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGF,Number=G,Type=Float,Description="Test Number=AGR in INFO">
+##INFO=<ID=XRI,Number=R,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAI,Number=A,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGI,Number=G,Type=Integer,Description="Test Number=AGR in INFO">
+##INFO=<ID=XRS,Number=R,Type=String,Description="Test Number=AGR in INFO">
+##INFO=<ID=XAS,Number=A,Type=String,Description="Test Number=AGR in INFO">
+##INFO=<ID=XGS,Number=G,Type=String,Description="Test Number=AGR in INFO">
+##FORMAT=<ID=FRF,Number=R,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAF,Number=A,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGF,Number=G,Type=Float,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FRI,Number=R,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAI,Number=A,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGI,Number=G,Type=Integer,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FRS,Number=R,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FAS,Number=A,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FGS,Number=G,Type=String,Description="Test Number=AGR in FORMAT">
+##FORMAT=<ID=FSTR,Number=1,Type=String,Description="Test String in FORMAT">
+##INFO=<ID=ISTR,Number=1,Type=String,Description="Test String in INFO">
+##INFO=<ID=INDEL,Number=0,Type=Flag,Description="Indicates that the variant is an INDEL.">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Phred-scaled likelihood">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Depth">
+##contig=<ID=1,length=2147483647>
+##contig=<ID=2,length=2147483647>
+##contig=<ID=3,length=2147483647>
+##contig=<ID=4,length=2147483647>
+##contig=<ID=5,length=2147483647>
+##contig=<ID=20,length=2147483647>
+##FILTER=<ID=Test,Description="Test Filter">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	XY00001	BB
+2	101	.	ATTTTTTTTTTTTT	ATTTTTTTTTTTTTTT	999	PASS	INDEL;AN=4;AC=4	GT:DP	1/1:1	1/1:1
+2	114	.	TC	TTCC,TTC	999	PASS	INDEL;AN=4;AC=2,2	GT:DP	1/2:1	1/2:1
+2	115	.	C	T	999	PASS	INDEL;AN=4;AC=4	GT:DP	1/1:1	1/1:1
+20	3	.	G	CT	999	PASS	INDEL;AN=4;AC=2	GT	0/1	0/1
+20	3	.	GATG	GACT	999	PASS	INDEL;AN=4;AC=2	GT	1/0	1/0
+20	5	.	TGGG	TAC,TG,TGGGG,AC	.	PASS	INDEL;AN=4;AC=2,2,0,0	GT:PL:DP	1/2:1,2,3,4,5,6,7,8,9,10,11,12,13,14,15:1	1/2:1,2,3,4,5,6,7,8,9,10,11,12,13,14,15:1
+20	59	.	AG	.	999	PASS	AN=4	GT:PL:DP	0/0:0:4	0/0:0:4
+20	80	.	CACAG	CACAT	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	81	.	A	C	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	95	.	TCACCG	ACACCG	999	PASS	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	95	.	TCACCG	AAAAAA	999	Test	AN=4;AC=2	GT:PL:DP	0/1:255,0,255:13	0/1:255,0,255:13
+20	273	.	CAAAAAAAAAAAAAAAAAAAAA	CAAAAAAAAAAAAAAAAAAAAAAA,CAAAAAAAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=4;AC=2,2	GT:PL:DP	1/2:0,3,5,3,5,5:1	1/2:0,3,5,3,5,5:1
+20	274	.	AAAAAAAAA	AAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=0;AC=0	GT:PL:DP	./.:0,0,0:0	./.:0,0,0:0
+20	278	.	AAAAAAAAAAAAAAAAA	AAAAAAAAAAAAAAAAAAA	999	PASS	INDEL;AN=0;AC=0	GT:PL:DP	./.:0,0,0:0	./.:0,0,0:0
+3	10	.	GTGGAC	GTGGACACAC,GTGGACAC,GTGGACACACAC,GTGG,GTGGACACACACAC,ATGGACACACAC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+3	15	.	CACA	CAC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+4	21	.	ATTTTTTTTTTTTTTTC	ATTTTTTTTTTTTTTC,ATTTTTTTTTTTTTTTT,ATTTTTTTTTTTTTTTTC	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0
+5	22	.	A	AGA	999	PASS	INDEL;AN=0	GT:DP	./.:0	./.:0

test/reheader.samples3
@@ -1,0 +1,2 @@
+XY00002 BB\ B
+XY00001 A\ AA

test/reheader.samples4
@@ -1,0 +1,1 @@
+XY00002 BB

test/test.pl
@@ -191,6 +191,8 @@ test_vcf_concat($opts,in=>['concat.3.a','concat.3.b','concat.3.0','concat.3.c','
 test_vcf_reheader($opts,in=>'reheader',out=>'reheader.1.out',header=>'reheader.hdr');
 test_vcf_reheader($opts,in=>'reheader',out=>'reheader.2.out',samples=>'reheader.samples');
 test_vcf_reheader($opts,in=>'reheader',out=>'reheader.2.out',samples=>'reheader.samples2');
+test_vcf_reheader($opts,in=>'reheader',out=>'reheader.3.out',samples=>'reheader.samples3');
+test_vcf_reheader($opts,in=>'reheader',out=>'reheader.4.out',samples=>'reheader.samples4');
 test_rename_chrs($opts,in=>'annotate');
 test_vcf_convert($opts,in=>'convert',out=>'convert.gs.gt.gen',args=>'-g -,.');
 test_vcf_convert($opts,in=>'convert',out=>'convert.gs.gt.samples',args=>'-g .,-');